1cc38641aa5b872a73dfe166837a01e56730b52e
Christopher Platt <plattnotpratt@gmail.com>
2023-06-07 18:20:17 -0700

    cleared migration

--- prisma/migrations/20230105152659_update/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `Board` MODIFY `description` TEXT NULL;
-
--- AlterTable
-ALTER TABLE `User` MODIFY `fname` VARCHAR(64) NULL,
-    MODIFY `lname` VARCHAR(64) NULL;
--- prisma/migrations/20230107153213_update2/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `title` to the `Task` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Task` ADD COLUMN `description` TEXT NULL,
-    ADD COLUMN `title` VARCHAR(255) NOT NULL;
